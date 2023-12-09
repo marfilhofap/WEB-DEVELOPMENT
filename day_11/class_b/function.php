cd808a7354801dd5ababcc3411216b926a6a99e1
Marfilho Fidel Alvares Pereira <marfilhofap@gmail.com>
2023-12-09 15:52:17 +0900

    Update - day 11
    Insert - Class b

--- day_11/class_b/function.php
+++ day_11/class_b/function.php
@@ -3,7 +3,8 @@
 include('koneksaun.php');
 $kon = koneksaun();
 
-function select_tabela($naran_tabela){
+function select_tabela($naran_tabela)
+{
     global $kon;
 
     // Query
@@ -15,8 +16,24 @@ function select_tabela($naran_tabela){
     $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
 
     return $resultado;
-
 }
 
+function insert_estudante($naran_estudante, $sexo, $data_moris)
+{
+    global $kon;
 
-?>
+    $query = "INSERT INTO t_estudante(naran_estudante, sexo, data_moris) 
+    VALUES ('$naran_estudante', '$sexo', '$data_moris')";
+    $sql = $kon->prepare($query);
+    $sql->execute();
+}
+
+function insert_materia($materia)
+{
+    global $kon;
+
+    $query = "INSERT INTO t_materia(materia) 
+    VALUES ('$materia')";
+    $sql = $kon->prepare($query);
+    $sql->execute();
+}
--- day_11/class_b/materia.php
+++ day_11/class_b/materia.php
@@ -1,36 +1,76 @@
-<?php 
+<?php
 include('function.php');
-$dados = select_tabela('t_materia');
+$dados = select_tabela('t_materia order by materia ASC');
+
+if (isset($_POST['gravar'])) {
+    if (!empty($_POST['materia'])) {
+        $materia = $_POST['materia'];
+
+        $rejultadu = insert_materia($materia);
+
+        header('Location: materia.php');
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>CRUD</title>
 </head>
+
 <body>
-    
-<h1>Lista Materia</h1>
 
-<p><a href="index.php">Lista Estudante</a></p>
+    <?php if (!isset($_GET['insert'])) { ?>
 
-<table border="1">
-    <thead>
-        <td>No</td>
-        <td>Materia</td>
-    </thead>
-    <tbody>
-        <?php
-        $no = 1; 
-        foreach($dados as $a){
-        ?>
-        <tr>
-            <td><?= $no++?></td>
-            <td><?= $a['materia']?></td>
-        </tr>
-        <?php }?>
-    </tbody>
-</table>
+        <h1>Lista Materia</h1>
+
+        <p><a href="index.php">Lista Estudante</a></p>
+        <p><a href="materia.php?insert=true">Insert</a></p>
+
+        <table border="1">
+            <thead>
+                <td>No</td>
+                <td>Materia</td>
+            </thead>
+            <tbody>
+                <?php
+                $no = 1;
+                foreach ($dados as $a) {
+                ?>
+                    <tr>
+                        <td><?= $no++ ?></td>
+                        <td><?= $a['materia'] ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+
+    <?php
+    }
+    if (isset($_GET['insert']) && $_GET['insert'] == 'true') {
+    ?>
+
+        <h2>Insert dados Materia</h2>
+        <form action="materia.php" method="post">
+            <ul>
+                <li>
+                    <label for="materia">Materia:</label>
+                    <input type="text" name="materia" id="materia" required>
+                </li>
+
+                <li>
+                    <button type="submit" name="gravar">Save</button>
+                    <button><a href="materia.php">Kansela</a></button>
+                </li>
+            </ul>
+        </form>
+
+    <?php
+    }
+    ?>
 
 </body>
+
 </html>
